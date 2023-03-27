defba09dac59064148695e03ddf86d71a6cf5ca8
maikspikes <verheij.maik@gmail.com>
2023-03-27 20:49:05 +0200

    first commit

---
+++ MyFirstProjectUsingJava/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) throws Exception {
+        
+        int x = 5 ;
+        double y = 7;
+        double z = 57;
+        double u = x * z;
+        double d = Math.pow(x, y);
+
+        System.out.println(d);
+    }
+}
+
